fb6256d2ed92d973a15dc4f4cbac8f7e8d14a1fc
Ronit26x <150147594+Ronit26x@users.noreply.github.com>
2024-09-15 20:41:50 -0400

    Update convolve.cpp

--- src/convolve.cpp
+++ src/convolve.cpp
@@ -44,77 +44,60 @@ Result read(string filename) {
 	return ab;
 }
 
-vector<vector<int>> convolve(vector<vector<int>> A)
-{
-    // Gaussian filter
-    vector<vector<int>> filter = {
+// Define the 3x3 Gaussian kernel with integer values
+vector<vector<int>> gaussianKernel() {
+    return {
         {1, 2, 1},
         {2, 4, 2},
-        {1, 2, 1}};
-    int n = A.size();
-
-    for (int i = 0; i < n; i++)
-    {
-        A[i].insert(A[i].begin(), 0);
-        A[i].insert(A[i].end(), 0);
-    }
-    vector<int> temp(n+2, 0);
-    A.insert(A.begin(), temp);
-    A.insert(A.end(), temp);
-
-    vector<vector<int>> res(n * n, vector<int> (n,0));
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            // res[i][j]
-
-            // 00 01 02  x-1, y-1    x-1,y     x-1,y+1
-            // 10 11 12  x,y-1      x, y       x, y+1
-            // 20 21 22  x+1, y-1    x+1,y     x+1, y+1
-            int sum = 0;
-            sum += A[i - 1][j - 1]  * filter[0][0];
-            sum += A[i - 1][j]      * filter[0][1];
-            sum += A[i - 1][j + 1]  * filter[0][2];
-            sum += A[i][j - 1]      * filter[1][0];
-            sum += A[i][j]          * filter[1][1];
-            sum += A[i][j + 1]      * filter[1][2];
-            sum += A[i + 1][j - 1]  * filter[2][0];
-            sum += A[i + 1][j]      * filter[2][1];
-            sum += A[i + 1][j + 1]  * filter[2][2];
-            sum = sum / 16;
-            res[i - 1][j - 1] = (int)sum;
-        }
-    }
-    return res;
+        {1, 2, 1}
+    };
 }
 
-void printMatrix(vector< vector<int> > matrix) {
-	vector< vector<int> >::iterator it;
-	vector<int>::iterator inner;
-	for (it=matrix.begin(); it != matrix.end(); it++) {
-		for (inner = it->begin(); inner != it->end(); inner++) {
-			cout << *inner;
-			if(inner+1 != it->end()) {
-				cout << "\t";
-			}
-		}
-		cout << endl;
-	}
+vector<vector<int>> padMatrix(const vector<vector<int>>& matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    vector<vector<int>> padded(rows + 2, vector<int>(cols + 2, 0));
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            padded[i + 1][j + 1] = matrix[i][j];
+        }
+    }
+    return padded;
+}
+
+vector<vector<int>> convolve(const vector<vector<int>>& matrix) {
+    vector<vector<int>> kernel = gaussianKernel();
+    vector<vector<int>> padded = padMatrix(matrix);
+    int rows = padded.size();
+    int cols = padded[0].size();
+    vector<vector<int>> result(matrix.size(), vector<int>(matrix[0].size(), 0));
+
+    for (int i = 1; i < rows - 1; ++i) {
+        for (int j = 1; j < cols - 1; ++j) {
+            int sum = 0;
+            for (int ki = -1; ki <= 1; ++ki) {
+                for (int kj = -1; kj <= 1; ++kj) {
+                    sum += padded[i + ki][j + kj] * kernel[ki + 1][kj + 1];
+                }
+            }
+            result[i - 1][j - 1] = sum;
+        }
+    }
+    return result;
 }
 
 int main (int argc, char* argv[]) {
-	srand(time(0));
-	string filename;
-	if (argc < 3) {
-		filename = "2000.in";
-	} else {
-		filename = argv[2];
-	}
-	Result result = read (filename);
+    srand(time(0));
+    string filename;
+    if (argc < 3) {
+        filename = "2000.in";
+    } else {
+        filename = argv[2];
+    }
+    Result result = read(filename);
     parsec_roi_begin();
-	vector< vector<int> > C = convolve(result.A);
+    vector< vector<int> > C = convolve(result.A);
     parsec_roi_end();
-	//printMatrix(C);
-	return 0;
+    return 0;
 }
